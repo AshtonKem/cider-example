2b3df768b3a75cfe90301d6ee84690a0d178d9f7
Ashton Kemerling <ashtonkemerling@gmail.com>
2016-02-04 17:06:48 -0600

    Init

---
+++ test/cider_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns cider-example.core-test
+  (:require [clojure.test :refer :all]
+            [cider-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
